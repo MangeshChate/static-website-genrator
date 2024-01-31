983366af8a83d618a7a6d2781596a50b3feb3940
mangesh <mbchate08@gmail.com>
2024-01-31 18:53:52 +0530

    coomit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-
-function page() {
-  
+function Page() {
   return (
-    <div className=''>
-      <span className='text-blue-900 font-bold text-3xl'>Simple Static website Genrator</span>
+    <div className='h-[100vh]'>
+      <span className='text-blue-900 font-bold text-3xl'>Simple Static Website Generator</span>
+      <div className='grid grid-cols-4 gap-3 h-[90%] mt-3 '>
+        <div className='col-span-1 bg-blue-200 p-4 flex flex-col items-center rounded-xl shadow-xl'>
+          <span className='text-xl'>Drag an Drop Element from here</span>
+          <hr />
+        </div>
+        <div className="col-span-3 bg-blue-500 p-4 flex items-center flex-col rounded-xl shadow-xl">
+          <span className='text-xl '>Your static Website</span>
+          
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
