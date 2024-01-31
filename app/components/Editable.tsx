d89f8d61ac854b332c34145494605d02e4e6da42
mangesh <mbchate08@gmail.com>
2024-01-31 18:22:12 +0530

    commit

---
+++ app/components/Editable.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const Editable = () => {
+  return (
+    <div>
+      Editable section
+    </div>
+  )
+}
+
+export default Editable
---
+++ app/components/Tools.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const Tools = () => {
+  return (
+    <div>
+      Tools section
+    </div>
+  )
+}
+
+export default Tools
